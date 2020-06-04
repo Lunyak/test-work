0a75136adabc742a35abc4749b87cca096a23a02
Dr.Lunyak <wampwar11@gmail.com>
2020-06-04 15:49:09 +0300

    close burger menu

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 const slider = (function () {
   //const
-	const slider = document.getElementById("slider"); // основная обертка
-	const sliderContent = document.querySelector(".slider__content"); // обертка для контейнера слайдов и контролов
-	const sliderWrapper = document.querySelector(".slider__content-wrapper"); // контейнер для слайдов
-	const sliderMedia = document.querySelector(".slider__content-media"); // для медиа
-	const elements = document.querySelectorAll(".slider__content-item"); // обертка для слайда
-   const sliderContentControls = createHTMLElement(
-   "div",
-   "slider__content-controls"
+  const slider = document.getElementById("slider"); // основная обертка
+  const sliderContent = document.querySelector(".slider__content"); // обертка для контейнера слайдов и контролов
+  const sliderWrapper = document.querySelector(".slider__content-wrapper"); // контейнер для слайдов
+  const sliderMedia = document.querySelector(".slider__content-media"); // для медиа
+  const elements = document.querySelectorAll(".slider__content-item"); // обертка для слайда
+  const sliderContentControls = createHTMLElement(
+    "div",
+    "slider__content-controls"
   ); // блок контролов внутри sliderContent
   let dotsWrapper = null; // Обертка dots
   let prevButton = null; // Кнопки
@@ -18,7 +18,7 @@ const slider = (function () {
   let intervalId = null; //идентификатор setInterval
 
   // data
-  	const itemsInfo = {
+  const itemsInfo = {
     offset: 0, // смещение контейнера со слайдами относительно начальной точки (первый слайд)
     position: {
       current: 0, // номер текущего слайда
@@ -272,233 +272,248 @@ const slider = (function () {
 
 slider.init({
   // intervalSpeed: 1000,
-currentItem: 0,
-buttons: true,
-dots: true,
+  currentItem: 0,
+  buttons: true,
+  dots: true,
 });
 
-
-
-
-
-
-
-
+// --- закрытие под меню  ---//
 
 let isClosed = true;
 
 $(document).ready(function () {
-   $(".help__burger").on("click", function () {
-   if (isClosed) {
+  $(".help__burger").on("click", function () {
+    if (isClosed) {
       $(".drop__menu-burger").show();
       $(".help__burger").removeClass("drop__arrow-close");
-		$(".help__burger").addClass("drop__arrow-open");
-		$('.nav__burger-wrap').css({
-			'height':'519px'
-		});
+      $(".help__burger").addClass("drop__arrow-open");
+      $(".nav__burger-wrap").css({
+        height: "519px",
+      });
       isClosed = false;
-   } else {
+    } else {
       $(".drop__menu-burger").hide();
       $(".help__burger").removeClass("drop__arrow-open");
       $(".help__burger").addClass("drop__arrow-close");
-		$('.nav__burger-wrap').css({
-			'height':'355px'
-		});
-		isClosed = true;
-   }
-	});
+      $(".nav__burger-wrap").css({
+        height: "355px",
+      });
+      isClosed = true;
+    }
+  });
 });
+
+// --- закрытие бургер меню ---//
 
 
 let burger = true;
 
 $(document).ready(function () {
-   $(".burger").on("click", function () {
-   if (burger) {
-		$(".nav__burger-wrap").show();
-		$(".burger").removeClass("burger__statick");
-		$(".burger").addClass("burger__click");
+  $(".burger").on("click", function () {
+    if (burger) {
+      $(".nav__burger-wrap").show();
+      $(".burger").removeClass("burger__statick");
+      $(".burger").addClass("burger__click");
       burger = false;
-   } else {
-		$(".nav__burger-wrap").hide();
-		$(".burger").addClass("burger__statick");
+    } else {
+      $(".nav__burger-wrap").hide();
+      $(".burger").addClass("burger__statick");
       $(".burger").removeClass("burger__click");
-		burger = true;
-   }
-	});
-});
-
-$(document).ready(function(){
-   $('.popup__btn').on('click', function(){
-      $('.overlay').show(200)
-   });
-   $('.popup-close').on('click', function(){
-      $('.overlay').hide(200);
-   });
+      burger = true;
+    }
+  });
 });
 
 
+
+$(document).ready(function () {
+  $(".popup__btn").on("click", function () {
+    $(".overlay").show(200);
+  });
+  $(".popup-close").on("click", function () {
+    $(".overlay").hide(200);
+  });
+});
 
 //  первая карточка  //
-
 
 let linkOne = true;
 
 $(document).ready(function () {
+  $(".learn__more-one").on("click", function () {
+    if (linkOne) {
+      $(".toggle__img-one").removeClass("one"); // меняем цифру на картинку
+      $(".toggle__img-one").addClass("img__down"); // меняем цифру на картинку
+      $(".transform__one").addClass("padding__drop");
+      $(".img__down").show();
+      $(".transform__one").css({
+        "padding-left": "70px",
+      });
+      $(".slider__content-wrapper").css({
+        left: "80px",
+      });
 
-   $(".learn__more-one").on("click", function () {
-   if (linkOne) {
-		$(".toggle__img-one").removeClass("one"); // меняем цифру на картинку
-		$(".toggle__img-one").addClass("img__down"); // меняем цифру на картинку
-		$(".transform__one").addClass("padding__drop");
-		$(".img__down").show();
-		$('.transform__one').css({
-			'padding-left':'70px',
-		});
-		$('.slider__content-wrapper').css({
-			'left':'80px',
-		});
-
-		$('.text__h2-one').text("Search Data");
-		$('.text__paragrath-one').text("Each time she picked one she thought that she could see an even more beautiful one a little way off, and she ran after it, going further and further into the woods.");
+      $(".text__h2-one").text("Search Data");
+      $(".text__paragrath-one").text(
+        "Each time she picked one she thought that she could see an even more beautiful one a little way off, and she ran after it, going further and further into the woods."
+      );
       linkOne = false;
-   } else {
-		$(".toggle__img-one").removeClass("img__down"); // меняем цифру на картинку
-		$(".toggle__img-one").addClass("one"); // меняем цифру на картинку
-		$(".transform__one").removeClass("padding__drop");
-		$(".img__down").hide();
-		$(".transform__one").css({
-			'padding-left':'0px',
-		});
-		$('.slider__content-wrapper').css({
-			'left':'150px',
-		});
-		$('.text__h2-one').text("First Feature");
-		$('.text__paragrath-one').text("SDon’t worry if your data is very large, the Data Warehoue provides a search engine, which is useful for making it easier to find data effectively saving time.");
-		linkOne = true;
-   }
-	});
+    } else {
+      $(".toggle__img-one").removeClass("img__down"); // меняем цифру на картинку
+      $(".toggle__img-one").addClass("one"); // меняем цифру на картинку
+      $(".transform__one").removeClass("padding__drop");
+      $(".img__down").hide();
+      $(".transform__one").css({
+        "padding-left": "0px",
+      });
+      $(".slider__content-wrapper").css({
+        left: "150px",
+      });
+      $(".text__h2-one").text("First Feature");
+      $(".text__paragrath-one").text(
+        "SDon’t worry if your data is very large, the Data Warehoue provides a search engine, which is useful for making it easier to find data effectively saving time."
+      );
+      linkOne = true;
+    }
+  });
 });
 
-
 //   вторая карточка  //
-
-
 
 let linkTwo = true;
 
 $(document).ready(function () {
-   $(".learn__more-two").on("click", function () {
-   if (linkTwo) {
-		$(".toggle__img-two").removeClass("two"); // меняем цифру на картинку
-		$(".toggle__img-two").addClass("img__up"); // меняем цифру на картинку
-		$(".img__up").show();
-		$('.card').css({
-			'margin-right':'80px'
-		});
-		$('.transform__two').css({
-			'padding-left':'70px',
-		});
-		$('.text__h2-two').text("Search Data");
-		$('.text__paragrath-two').text("Each time she picked one she thought that she could see an even more beautiful one a little way off, and she ran after it, going further and further into the woods.");
+  $(".learn__more-two").on("click", function () {
+    if (linkTwo) {
+      $(".toggle__img-two").removeClass("two"); // меняем цифру на картинку
+      $(".toggle__img-two").addClass("img__up"); // меняем цифру на картинку
+      $(".img__up").show();
+      $(".card").css({
+        "margin-right": "80px",
+      });
+      $(".transform__two").css({
+        "padding-left": "70px",
+      });
+      $(".text__h2-two").text("Search Data");
+      $(".text__paragrath-two").text(
+        "Each time she picked one she thought that she could see an even more beautiful one a little way off, and she ran after it, going further and further into the woods."
+      );
       linkTwo = false;
-   } else {
-		$(".toggle__img-two").removeClass("img__up"); // меняем цифру на картинку
-		$(".toggle__img-two").addClass("two"); // меняем цифру на картинку
-		$(".img__up").hide();
-		$('.card').css({
-			'margin-right':'150px'
-		});
-		$('.transform__two').css({
-			'padding-left':'0px',
-		});
-		$('.text__h2-two').text("Second Feature");
-		$('.text__paragrath-two').text("SDon’t worry if your data is very large, the Data Warehoue provides a search engine, which is useful for making it easier to find data effectively saving time.");
-		linkTwo = true;
-   }
-	});
+    } else {
+      $(".toggle__img-two").removeClass("img__up"); // меняем цифру на картинку
+      $(".toggle__img-two").addClass("two"); // меняем цифру на картинку
+      $(".img__up").hide();
+      $(".card").css({
+        "margin-right": "150px",
+      });
+      $(".transform__two").css({
+        "padding-left": "0px",
+      });
+      $(".text__h2-two").text("Second Feature");
+      $(".text__paragrath-two").text(
+        "SDon’t worry if your data is very large, the Data Warehoue provides a search engine, which is useful for making it easier to find data effectively saving time."
+      );
+      linkTwo = true;
+    }
+  });
 });
 
-
 //   третья карточка  //
-
 
 let linkThree = true;
 
 $(document).ready(function () {
+  $(".learn__more-three").on("click", function () {
+    if (linkThree) {
+      $(".toggle__img-three").removeClass("three"); // меняем цифру на картинку
+      $(".toggle__img-three").addClass("img__down"); // меняем цифру на картинку
+      $(".img__down").show();
+      $(".transform__three").css({
+        "padding-left": "70px",
+      });
+      $(".slider__content-wrapper").css({
+        left: "80px",
+      });
 
-   $(".learn__more-three").on("click", function () {
-   if (linkThree) {
-		$(".toggle__img-three").removeClass("three"); // меняем цифру на картинку
-		$(".toggle__img-three").addClass("img__down"); // меняем цифру на картинку
-		$(".img__down").show();
-		$('.transform__three').css({
-			'padding-left':'70px',
-		});
-		$('.slider__content-wrapper').css({
-			'left':'80px',
-		});
-
-		$('.text__h2-three').text("Search Data");
-		$('.text__paragrath-three').text("Each time she picked one she thought that she could see an even more beautiful one a little way off, and she ran after it, going further and further into the woods.");
+      $(".text__h2-three").text("Search Data");
+      $(".text__paragrath-three").text(
+        "Each time she picked one she thought that she could see an even more beautiful one a little way off, and she ran after it, going further and further into the woods."
+      );
       linkThree = false;
-   } else {
-		$(".toggle__img-three").removeClass("img__down"); // меняем цифру на картинку
-		$(".toggle__img-three").addClass("three"); // меняем цифру на картинку
-		$(".img__down").hide();
-		$(".transform__three").css({
-			'padding-left':'0px',
-		});
-		$('.slider__content-wrapper').css({
-			'left':'150px',
-		});
-		$('.text__h2-three').text("First Feature");
-		$('.text__paragrath-three').text("SDon’t worry if your data is very large, the Data Warehoue provides a search engine, which is useful for making it easier to find data effectively saving time.");
-		linkThree = true;
-   }
-	});
+    } else {
+      $(".toggle__img-three").removeClass("img__down"); // меняем цифру на картинку
+      $(".toggle__img-three").addClass("three"); // меняем цифру на картинку
+      $(".img__down").hide();
+      $(".transform__three").css({
+        "padding-left": "0px",
+      });
+      $(".slider__content-wrapper").css({
+        left: "150px",
+      });
+      $(".text__h2-three").text("First Feature");
+      $(".text__paragrath-three").text(
+        "SDon’t worry if your data is very large, the Data Warehoue provides a search engine, which is useful for making it easier to find data effectively saving time."
+      );
+      linkThree = true;
+    }
+  });
 });
-
-
 
 //   четвертая карточка  //
 
 let linkFore = true;
 
 $(document).ready(function () {
-   $(".learn__more-fore").on("click", function () {
-   if (linkFore) {
-		$(".toggle__img-fore").removeClass("fore"); // меняем цифру на картинку
-		$(".toggle__img-fore").addClass("img__up"); // меняем цифру на картинку
-		$(".img__up").show();
-		$('.card').css({
-			'margin-right':'80px'
-		});
-		$('.transform__fore').css({
-			'padding-left':'70px',
-		});
-		$('.new').css({ // new
-			'left':'185px',
-		});
-		$('.text__h2-fore').text("Search Data");
-		$('.text__paragrath-fore').text("Each time she picked one she thought that she could see an even more beautiful one a little way off, and she ran after it, going further and further into the woods.");
+  $(".learn__more-fore").on("click", function () {
+    if (linkFore) {
+      $(".toggle__img-fore").removeClass("fore"); // меняем цифру на картинку
+      $(".toggle__img-fore").addClass("img__up"); // меняем цифру на картинку
+      $(".img__up").show();
+      $(".card").css({
+        "margin-right": "80px",
+      });
+      $(".transform__fore").css({
+        "padding-left": "70px",
+      });
+      $(".new").css({
+        // new
+        left: "185px",
+      });
+      $(".text__h2-fore").text("Search Data");
+      $(".text__paragrath-fore").text(
+        "Each time she picked one she thought that she could see an even more beautiful one a little way off, and she ran after it, going further and further into the woods."
+      );
       linkFore = false;
-   } else {
-		$(".toggle__img-fore").removeClass("img__up"); // меняем цифру на картинку
-		$(".toggle__img-fore").addClass("fore"); // меняем цифру на картинку
-		$(".img__up").hide();
-		$('.card').css({
-			'margin-right':'150px'
-		});
-		$('.transform__fore').css({
-			'padding-left':'0px',
-		});
-		$('.new').css({ // new
-			'left':'115px',
-		});
-		$('.text__h2-fore').text("Second Feature");
-		$('.text__paragrath-fore').text("SDon’t worry if your data is very large, the Data Warehoue provides a search engine, which is useful for making it easier to find data effectively saving time.");
-		linkFore = true;
-   }
-	});
+    } else {
+      $(".toggle__img-fore").removeClass("img__up"); // меняем цифру на картинку
+      $(".toggle__img-fore").addClass("fore"); // меняем цифру на картинку
+      $(".img__up").hide();
+      $(".card").css({
+        "margin-right": "150px",
+      });
+      $(".transform__fore").css({
+        "padding-left": "0px",
+      });
+      $(".new").css({
+        // new
+        left: "115px",
+      });
+      $(".text__h2-fore").text("Second Feature");
+      $(".text__paragrath-fore").text(
+        "SDon’t worry if your data is very large, the Data Warehoue provides a search engine, which is useful for making it easier to find data effectively saving time."
+      );
+      linkFore = true;
+    }
+  });
+});
+
+$(window).resize(function () {
+  const windowWidth = $(window).width();
+
+  if (windowWidth > 1200) {
+    $(".nav__burger-wrap").hide();
+    $(".burger").removeClass("burger__click");
+    $(".burger").addClass("burger__statick");
+    burger = true;
+  }
 });
